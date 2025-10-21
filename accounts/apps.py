84508c267a9cc92452138513aa1abe67bf3a508c
ViictorDantas <viictordantaz@gmail.com>
2025-10-21 18:45:56 -0300

    remove arquivos e configurações da aplicação de contas e empresas

--- accounts/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountsConfig(AppConfig):
-    """
-    Configuração da aplicação de contas.
-    """
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
